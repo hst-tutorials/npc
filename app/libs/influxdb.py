17d4b08090a4281d147e2b181e591a0f4c4dc499
Heiko Matthies <heiko.matthies@asap.de>
2022-08-18 10:31:14 +0200

    rewrote influxdb logic

--- app/libs/influxdb.py
+++ app/libs/influxdb.py
@@ -1,14 +1,19 @@
-#!/usr/bin/python3
-
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
 
 def writeToInflux(config, data, bucket):
-    influxHost = config['influxdb']['influxhost']
-    influxToken = config['influxdb']['influxtoken']
-    influxOrg = config['influxdb']['influxorg']
-
-    with InfluxDBClient(url=influxHost, token=influxToken, org=influxOrg) as client:
-        write_api = client.write_api(write_options=SYNCHRONOUS)
-        write_api.write(bucket, influxOrg, data)
+    
+    influxHost = config['influxdb']['host']
+    influxToken = config['influxdb']['token']
+    influxOrg = config['influxdb']['org']
+    
+    if config['influxdb']['enabled'] == "True":
+            log.writeLog("Writing data to influxdb", "INFO", "stdout")
+            with InfluxDBClient(url=influxHost, token=influxToken, org=influxOrg) as client:
+                write_api = client.write_api(write_options=SYNCHRONOUS)
+                write_api.write(bucket, influxOrg, data)
+    else:  
+        print(data)
+    
+    
